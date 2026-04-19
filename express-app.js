f07abaccc6be24245c80375841471059feba69e9
Vinay <vnyone7@gmail.com>
2026-04-19 15:26:07 +0530

    log the base url

--- express-app.js
+++ express-app.js
@@ -6,6 +6,8 @@ const referenceTable = require("./referenceTable");
 const BASE_URL = process.env.BASE_URL || "http://localhost:8000";
 
 module.exports = async (app, db) => {
+  console.log("BASE_URL:", BASE_URL);
+  
   // Middlewares
   app.use(express.json({ limit: "1mb" }));
   app.use(express.urlencoded({ extended: true, limit: "1mb" }));
